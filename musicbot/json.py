67597577f15b851dd013dbcde23f2135ed83b89c
itsTheFae <itsthefae@gmail.com>
2024-01-19 08:28:07 -0800

    Add typing for event_emitter.py and json.py

--- musicbot/json.py
+++ musicbot/json.py
@@ -1,26 +1,31 @@
 import json
+import pathlib
 import logging
+from typing import Any, Dict
 
 log = logging.getLogger(__name__)
 
 
 class Json:
-    def __init__(self, json_file):
+    def __init__(self, json_file: pathlib.Path) -> None:
         log.debug("Init JSON obj with {0}".format(json_file))
         self.file = json_file
         self.data = self.parse()
 
-    def parse(self):
+    def parse(self) -> Dict[str, Any]:
         """Parse the file as JSON"""
+        parsed = {}
         with open(self.file, encoding="utf-8") as data:
             try:
                 parsed = json.load(data)
+                if type(parsed) is not dict:
+                    raise TypeError("Parsed information must be of type Dict[str, Any]")
             except Exception:
                 log.error("Error parsing {0} as JSON".format(self.file), exc_info=True)
                 parsed = {}
         return parsed
 
-    def get(self, item, fallback=None):
+    def get(self, item: str , fallback: Any = None) -> Any:
         """Gets an item from a JSON file"""
         try:
             data = self.data[item]
@@ -31,7 +36,7 @@ class Json:
 
 
 class I18nJson(Json):
-    def get(self, item, fallback=None):
+    def get(self, item: str, fallback: Any = None) -> Any:
         try:
             data = self.data[item]
         except KeyError:
--- musicbot/lib/event_emitter.py
+++ musicbot/lib/event_emitter.py
@@ -1,14 +1,18 @@
 import asyncio
 import traceback
 import collections
+from typing import Callable, DefaultDict, Dict, List, Any
 
+EventCallback = Callable[..., Any]
+EventList = List[EventCallback]
+EventDict = DefaultDict[str, EventList]
 
 class EventEmitter:
-    def __init__(self):
-        self._events = collections.defaultdict(list)
-        self.loop = asyncio.get_event_loop()
+    def __init__(self: "EventEmitter") -> None:
+        self._events: EventDict = collections.defaultdict(list)
+        self.loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
 
-    def emit(self, event, *args, **kwargs):
+    def emit(self, event: str, *args: List[Any], **kwargs: Dict[str, Any]) -> None:
         if event not in self._events:
             return
 
@@ -22,11 +26,11 @@ class EventEmitter:
             except Exception:
                 traceback.print_exc()
 
-    def on(self, event, cb):
+    def on(self, event: str, cb: EventCallback) -> "EventEmitter":
         self._events[event].append(cb)
         return self
 
-    def off(self, event, cb):
+    def off(self, event: str, cb: EventCallback) -> "EventEmitter":
         self._events[event].remove(cb)
 
         if not self._events[event]:
@@ -34,8 +38,8 @@ class EventEmitter:
 
         return self
 
-    def once(self, event, cb):
-        def callback(*args, **kwargs):
+    def once(self, event: str, cb: EventCallback) -> "EventEmitter":
+        def callback(*args: List[Any], **kwargs: Dict[str, Any]) -> Any:
             self.off(event, callback)
             return cb(*args, **kwargs)
 
