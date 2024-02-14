edb939ed2934979cff0be270fac730c54a468448
itsTheFae <itsthefae@gmail.com>
2024-02-14 06:57:19 -0800

    Update code_checks.sh

--- code_checks.sh
+++ code_checks.sh
@@ -1,9 +1,55 @@
 #!/bin/bash
 
+HELP_ME=0
 DO_FORMAT=0
-if [ "$1" == "format" ]; then
-    DO_FORMAT=1
+DO_SPELLING=0
+DO_OPTIONAL=0
+ONLY_BASE_CHECKS=0
+while [[ $# -gt 0 ]] ; do
+case $1 in
+    "format"|"--format")
+        DO_FORMAT=1
+        shift
+    ;;
+    "spell"|"spelling"|"--spelling")
+        DO_SPELLING=1
+        shift
+    ;;
+    "base")
+        # only check with black
+        ONLY_BASE_CHECKS=1
+        shift
+    ;;
+    "opt"|"optional")
+        DO_OPTIONAL=1
+        shift
+    ;;
+    "help"|"--help"|"-h")
+        HELP_ME=1
+        shift
+    ;;
+    *)
+        shift
+    ;;
+esac
+done
+
+
+if [ "$HELP_ME" == "1" ]; then
+    echo "$0"
+    echo "Code checks helper script."
+    echo ""
+    echo "Available Options:"
+    echo " -h or help   Show this message and exit"
+    echo " format       Run code format via isort and black automatically."
+    echo " spell        Run spelling check, if pylint is configured for it."
+    echo " base         Only run basic black check. Option format will still apply if set."
+    echo " optional     Adds extra checks. Currently only bandit."
+    echo ""
+
+    exit 0
 fi
+
 
 echo "Black on py38:"
 if [ "$DO_FORMAT" == "0" ]; then
@@ -13,6 +59,11 @@ else
 fi
 echo "----"
 echo ""
+
+if [ "$ONLY_BASE_CHECKS" == "1" ] ; then
+  echo "Skipping all other checks."
+  exit 0
+fi
 
 echo "isort imports:"
 if [ "$DO_FORMAT" == "0" ]; then
@@ -34,19 +85,24 @@ echo "----"
 echo ""
 
 echo "pylint:"
-pylint --recursive=y .
+if [ "$DO_SPELLING" == "1" ]; then
+    pylint --recursive=y --spelling-dict=en_US --spelling-private-dict-file=./tests/repo-dictionary.txt .
+else
+    pylint --recursive=y .
+fi
 echo "----"
 echo ""
 
 
+if [ "$DO_OPTIONAL" == "1" ]; then
+
+echo "Bandit:"
+bandit -r .
+echo "----"
 
 #echo "Checking for i18n data missing from source base..."
 #python ./tests/test_missing_i18n.py
 #echo "----"
 
-#echo "Bandit:"
-#bandit -r .
-#echo "----"
-
-
+fi
 
