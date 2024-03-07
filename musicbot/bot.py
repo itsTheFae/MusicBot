32c6cb4126cb08d23b145bc8783c7de98c309cf5
Snow <135516526+BabyBoySnow@users.noreply.github.com>
2024-03-07 14:13:55 -0600

    update self summon to pass message
    Added setting lnp in summon, which uses message so we need to pass it in our calls.

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -3515,7 +3515,7 @@ class MusicBot(discord.Client):
         await channel.typing()
 
         if not player and permissions.summonplay and channel.guild:
-            response = await self.cmd_summon(channel.guild, author)
+            response = await self.cmd_summon(channel.guild, author, message)
             if response:
                 if self.config.embeds:
                     content = self._gen_embed()
@@ -3767,6 +3767,7 @@ class MusicBot(discord.Client):
         guild: discord.Guild,
         author: discord.Member,
         permissions: PermissionGroup,
+        message: discord.Message,
         song_url: str,
     ) -> CommandResponse:
         """
@@ -3784,7 +3785,7 @@ class MusicBot(discord.Client):
         if _player:
             player = _player
         elif permissions.summonplay:
-            response = await self.cmd_summon(guild, author)
+            response = await self.cmd_summon(guild, author, message)
             if response:
                 if self.config.embeds:
                     content = self._gen_embed()
