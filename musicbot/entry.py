4b3e54f41d429e63351e40bfb0f5521215a4e227
itsTheFae <itsthefae@gmail.com>
2024-03-18 07:20:22 -0700

    dyslexic logic strikes again.  gotta love it.

--- musicbot/entry.py
+++ musicbot/entry.py
@@ -231,8 +231,8 @@ class URLPlaylistEntry(BasePlaylistEntry):
     def from_auto_playlist(self) -> bool:
         """Returns true if the entry has an author or a channel."""
         if self.author is not None or self.channel is not None:
-            return True
-        return False
+            return False
+        return True
 
     @property
     def url(self) -> str:
@@ -721,8 +721,8 @@ class StreamPlaylistEntry(BasePlaylistEntry):
     def from_auto_playlist(self) -> bool:
         """Returns true if the entry has an author or a channel."""
         if self.author is not None or self.channel is not None:
-            return True
-        return False
+            return False
+        return True
 
     @property
     def url(self) -> str:
