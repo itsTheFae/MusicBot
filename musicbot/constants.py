646acace395f3ebc0da23d8fc13ef8437421be61
Fae <itsthefae@gmail.com>
2025-01-28 09:37:24 -0800

    increase DL threads to 5

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -122,7 +122,7 @@ VOICE_CLIENT_RECONNECT_TIMEOUT: int = 5
 VOICE_CLIENT_MAX_RETRY_CONNECT: int = 5
 
 # Maximum number of threads MusicBot will use for downloading and extracting info.
-DEFAULT_MAX_INFO_DL_THREADS: int = 2
+DEFAULT_MAX_INFO_DL_THREADS: int = 5
 # Maximum number of seconds to wait for HEAD request on media files.
 DEFAULT_MAX_INFO_REQUEST_TIMEOUT: int = 10
 
