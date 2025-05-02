6b3b18a06151c41bc9bcd0ed864d34e6a839767a
Fae <itsthefae@gmail.com>
2025-05-02 06:30:42 -0700

    Remove unneeded abort.  on_message handles this. If mypy is complaining, make sure to update it.

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -5327,10 +5327,6 @@ class MusicBot(discord.Client):
         - Mention a user: `{command_prefix}remove @user` removes their entries.
         - Provide nothing: `{command_prefix}remove` removes the last song. (LIFO-style)
         """
-        # add abort if no player to make mypy happy
-        if not player:
-            log.warning(f"No player for {guild}!")
-            return
 
         if not player.playlist.entries:
             raise exceptions.CommandError("Nothing in the queue to remove!")
