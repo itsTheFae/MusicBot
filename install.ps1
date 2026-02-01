f0c6afcaad81db98303b15efdd187660e3c797e0
Fae <itsthefae@gmail.com>
2026-02-01 15:42:13 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -106,10 +106,22 @@ if (-Not (Get-Command winget -ErrorAction SilentlyContinue) )
     Invoke-WebRequest -Uri "https://aka.ms/getwinget" -OutFile "winget.msixbundle"
     $ProgressPreference = 'Continue'
     Add-AppxPackage "winget.msixbundle"
-    #Start-Process "winget.msixbundle"
 
-    Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -ErrorAction Stop
-
+    # if the above fails, fall back to the more manual way...
+    if (-Not (Get-Command winget -ErrorAction SilentlyContinue) ) {
+        "... Trying a more manual install for winget..."
+        Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Destination "WinGet.msixbundle"
+        Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/latest/download/DesktopAppInstaller_Dependencies.zip" -Destination "DesktopAppInstaller_Dependencies.zip"
+        Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/latest/download/e53e159d00e04f729cc2180cffd1c02e_License1.xml" -Destination "license.xml"
+        Expand-Archive -Path "DesktopAppInstaller_Dependencies.zip"
+        Add-AppxPackage "DesktopAppInstaller_Dependencies\x64\Microsoft.UI.Xaml*x64.appx"
+        Add-AppxPackage "DesktopAppInstaller_Dependencies\x64\Microsoft.VCLibs*x64.appx"
+        Add-AppxPackage "DesktopAppInstaller_Dependencies\x64\Microsoft.WindowsAppRuntime*x64.appx"
+        Add-AppxProvisionedPackage -Online -PackagePath .\WinGet.msixbundle -LicensePath .\license.xml
+        Get-AppPackage *Microsoft.DesktopAppInstaller*|select Name,PackageFullName
+        winget --info
+        Remove-Item -Path "WinGet.msixbundle", "DesktopAppInstaller_Dependencies.zip", "DesktopAppInstaller_Dependencies", "license.xml" -Recurse -Force
+    }
 
     # check if winget is available post-install.
     if (-Not (Get-Command winget -ErrorAction SilentlyContinue) ) {
